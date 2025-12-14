3de3033b784179521809c9ebef46c6a8869048ef
TheBeardedWonder <the@beardedwonder.com>
2025-12-13 23:09:28 -0800

    feat(client): add income statement endpoint

---
+++ client/income_statement.go
@@ -0,0 +1,51 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/masonJamesWheeler/alpha-vantage-go-wrapper/models"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// GetIncomeStatement retrieves annual and quarterly income statements for the given symbol.
+// The endpoint requires function=INCOME_STATEMENT and a stock symbol.
+func (c *Client) GetIncomeStatement(params models.IncomeStatementParams) (*models.IncomeStatementResponse, error) {
+	symbol := strings.TrimSpace(params.Symbol)
+	if symbol == "" {
+		return nil, fmt.Errorf("symbol is required")
+	}
+
+	queryParams := url.Values{}
+	queryParams.Add("function", "INCOME_STATEMENT")
+	queryParams.Add("symbol", symbol)
+	queryParams.Add("apikey", c.apiKey)
+
+	resp, err := http.Get(alphaVantageURL + "?" + queryParams.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := detectAPIMessage(data); err != nil {
+		return nil, err
+	}
+
+	var statement models.IncomeStatementResponse
+	if err := json.Unmarshal(data, &statement); err != nil {
+		return nil, err
+	}
+
+	if statement.Symbol == "" {
+		return nil, fmt.Errorf("failed to retrieve income statement for symbol %s", symbol)
+	}
+
+	return &statement, nil
+}
